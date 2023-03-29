702334aca8543beca4706df0a156da5c312ece49
malikhendry <gpd_87@hotmail.com>
2023-03-29 21:20:25 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./views/LoginPage";
+import PostPageAdd from "./views/PostPageAdd";
+import PostPageDetails from "./views/PostPageDetails";
+import PostPageHome from "./views/PostPageHome";
+import PostPageUpdate from "./views/PostPageUpdate";
+import SignUpPage from "./views/SignUpPage";
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PostPageHome />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/add" element={<PostPageAdd />} />
+        <Route path="/post/:id" element={<PostPageDetails />} />
+        <Route path="/update/:id" element={<PostPageUpdate />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+
+
